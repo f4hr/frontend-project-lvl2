0c206052d9ea946e6492fcc8f9ab6120636a97c4
Ilya Fakhrutdinov <ilyaf4hr@gmail.com>
2021-02-26 22:55:41 +0500

    Step 3. Add flat JSON difference generation

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,17 @@
 #!/usr/bin/env node
 
-import genDiff from '../src/main.js';
+import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
-genDiff();
+const program = new Command();
+
+program
+  .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .action((filepath1, filepath2, format) => {
+    console.log(genDiff(filepath1, filepath2, format));
+  });
+
+program.parse();
