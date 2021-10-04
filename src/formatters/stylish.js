0c820c43424971e7a2ff2074c5ec9d52936d5f18
Ilya Fakhrutdinov <bekamez@gmail.com>
2021-10-04 15:38:32 +0500

    Refactor stylish formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,55 +1,60 @@
-const stylishDefaultOptions = {
+import _ from 'lodash';
+
+const defaultOptions = {
   replacer: ' ',
   spacesCount: 4,
 };
 
-const stylishStatusValues = {
-  removed: '- ',
-  added: '+ ',
-  unchanged: '  ',
-  complex: '  ',
+const buildLine = (key, status, value, indent) => {
+  switch (status) {
+    case 'added':
+      return `${indent}+ ${key}: ${value}`;
+    case 'removed':
+      return `${indent}- ${key}: ${value}`;
+    case 'unchanged':
+    case 'nested':
+      return `${indent}  ${key}: ${value}`;
+    default:
+      throw new Error(`Unknown status "${status}"`);
+  }
 };
 
-const buildLine = (status, key, value, indent) => `${indent}${stylishStatusValues[status]}${key}: ${value}`;
-
-export default (data, options = stylishDefaultOptions) => {
-  const currentOptions = { ...stylishDefaultOptions, ...options };
+export default (data, options = defaultOptions) => {
+  const currentOptions = { ...defaultOptions, ...options };
   const { replacer, spacesCount } = currentOptions;
   const indent = replacer.repeat(spacesCount);
-  const indentForValue = indent.substring(0, indent.length - 2);
+  const valueIndent = indent.substring(0, indent.length - 2);
 
-  const iter = (currentValue, status = 'unchanged', depth) => {
-    if (typeof currentValue !== 'object' || currentValue === null) {
-      return `${currentValue}`;
-    }
+  const iter = (tree, depth) => {
+    if (!_.isObject(tree)) return String(tree);
 
     const delimiter = `\n${indent.repeat(depth)}`;
 
-    if (status !== 'updated' && status !== 'complex') {
-      const keys = Object.keys(currentValue);
+    if (_.isPlainObject(tree)) {
+      const keys = Object.keys(tree);
 
       const lines = keys.map((key) => {
-        const value = iter(currentValue[key], 'unchanged', depth + 1);
+        const value = iter(tree[key], depth + 1);
 
-        return buildLine('unchanged', key, value, indentForValue);
+        return buildLine(key, 'unchanged', value, valueIndent);
       });
 
       return ['{', ...lines, '}'].join(delimiter);
     }
 
-    const lines = currentValue.reduce((acc, { status: stat, key, value }) => {
+    const lines = tree.reduce((acc, { status: stat, key, value }) => {
       if (stat === 'updated') {
-        const val1 = iter(value[0], 'unchanged', depth + 1);
-        const val2 = iter(value[1], 'unchanged', depth + 1);
+        const val1 = iter(value[0], depth + 1);
+        const val2 = iter(value[1], depth + 1);
 
-        const line1 = buildLine('removed', key, val1, indentForValue);
-        const line2 = buildLine('added', key, val2, indentForValue);
+        const line1 = buildLine(key, 'removed', val1, valueIndent);
+        const line2 = buildLine(key, 'added', val2, valueIndent);
 
         return [...acc, line1, line2];
       }
 
-      const val = iter(value, stat, depth + 1);
-      const line = buildLine(stat, key, val, indentForValue);
+      const val = iter(value, depth + 1);
+      const line = buildLine(key, stat, val, valueIndent);
 
       return [...acc, line];
     }, []);
@@ -57,5 +62,5 @@ export default (data, options = stylishDefaultOptions) => {
     return ['{', ...lines, '}'].join(delimiter);
   };
 
-  return iter(data, 'updated', 0);
+  return iter(data, 0);
 };
