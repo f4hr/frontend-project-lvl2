22e6a3f88305bd0d3bdaad7f696d2cbb281cbb13
Ilya Fakhrutdinov <bekamez@gmail.com>
2021-10-04 20:54:17 +0500

    Refactor stylish formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -5,62 +5,47 @@ const defaultOptions = {
   spacesCount: 4,
 };
 
-const buildLine = (key, status, value, indent) => {
-  switch (status) {
-    case 'added':
-      return `${indent}+ ${key}: ${value}`;
-    case 'removed':
-      return `${indent}- ${key}: ${value}`;
-    case 'unchanged':
-    case 'nested':
-      return `${indent}  ${key}: ${value}`;
-    default:
-      throw new Error(`Unknown status "${status}"`);
-  }
-};
-
 export default (data, options = defaultOptions) => {
   const currentOptions = { ...defaultOptions, ...options };
   const { replacer, spacesCount } = currentOptions;
-  const indent = replacer.repeat(spacesCount);
-  const valueIndent = indent.substring(0, indent.length - 2);
+  const baseIndent = replacer.repeat(spacesCount);
+  const valIndent = baseIndent.substring(0, baseIndent.length - 2);
 
-  const iter = (tree, depth) => {
+  const iter = (tree, depth = 0) => {
     if (!_.isObject(tree)) return String(tree);
 
-    const delimiter = `\n${indent.repeat(depth)}`;
+    const delimiter = `\n${baseIndent.repeat(depth)}`;
 
     if (_.isPlainObject(tree)) {
-      const keys = Object.keys(tree);
-
-      const lines = keys.map((key) => {
-        const value = iter(tree[key], depth + 1);
-
-        return buildLine(key, 'unchanged', value, valueIndent);
-      });
+      const lines = _.keys(tree).map((key) => `${valIndent}  ${key}: ${iter(tree[key], depth + 1)}`);
 
       return ['{', ...lines, '}'].join(delimiter);
     }
 
-    const lines = tree.reduce((acc, { status: stat, key, value }) => {
-      if (stat === 'updated') {
-        const val1 = iter(value[0], depth + 1);
-        const val2 = iter(value[1], depth + 1);
+    const lines = tree.map(({ status, key, value }) => {
+      switch (status) {
+        case 'updated': {
+          const [oldVal, newVal] = value;
 
-        const line1 = buildLine(key, 'removed', val1, valueIndent);
-        const line2 = buildLine(key, 'added', val2, valueIndent);
+          const line1 = `${valIndent}- ${key}: ${iter(oldVal, depth + 1)}`;
+          const line2 = `${valIndent}+ ${key}: ${iter(newVal, depth + 1)}`;
 
-        return [...acc, line1, line2];
+          return [line1, line2].join(delimiter);
+        }
+        case 'added':
+          return `${valIndent}+ ${key}: ${iter(value, depth + 1)}`;
+        case 'removed':
+          return `${valIndent}- ${key}: ${iter(value, depth + 1)}`;
+        case 'unchanged':
+        case 'nested':
+          return `${valIndent}  ${key}: ${iter(value, depth + 1)}`;
+        default:
+          throw new Error(`Unknown status "${status}"`);
       }
-
-      const val = iter(value, depth + 1);
-      const line = buildLine(key, stat, val, valueIndent);
-
-      return [...acc, line];
-    }, []);
+    });
 
     return ['{', ...lines, '}'].join(delimiter);
   };
 
-  return iter(data, 0);
+  return iter(data);
 };
