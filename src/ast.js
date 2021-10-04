87fdd9551233c2c50f813b1de76be83196ff4761
Ilya Fakhrutdinov <bekamez@gmail.com>
2021-10-04 16:22:08 +0500

    Replace sort with immutable version

--- src/ast.js
+++ src/ast.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 
 const getAst = (obj1, obj2) => {
-  const combinedKeys = _.union(_.keys(obj1), _.keys(obj2)).sort();
+  const combinedKeys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)), (obj) => obj);
 
   return combinedKeys.map((key) => {
     const oldVal = obj1[key];
